fa0da0b5bd3f7b5f494cc6e37ecb3181cd4194d3
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-16 06:41:08 +0530

    done

--- vpc_module/vpc.tf
+++ vpc_module/vpc.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "main" {
   cidr_block       =  var.vpc_cidr_block
   instance_tenancy = "default"
-  //enable_dns_hostnames = true
+  enable_dns_hostnames = true
 
   tags = merge(local.common_tags,
    {
